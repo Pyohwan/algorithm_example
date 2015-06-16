1bdc0083b61699cbedea4064f50a1bcbc4b64f23
Pyohwan <phjang1983@daum.net>
2015-06-16 22:55:20 +0900

    Added test methods.

--- test/algorithm/example/SortTest.java
+++ test/algorithm/example/SortTest.java
@@ -1,9 +1,14 @@
 package algorithm.example;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
 
 public class SortTest {
 
@@ -14,5 +19,32 @@ public class SortTest {
 		
 		Arrays.sort(numbers);
 		assertArrayEquals(expected, numbers);
+	}
+	
+	@Test
+	public void sortObjects() {
+		final String[] strings = {"z", "x", "y", "abc", "zzz", "zazzy"};
+		final String[] expected = {"abc", "x", "y", "z", "zazzy", "zzz"};
+		
+		Arrays.sort(strings);
+		assertArrayEquals(expected, strings);
+	}
+	
+	public class ReverseNumericalOrder implements Comparator<Integer> {
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			// TODO Auto-generated method stub
+			return o2 - o1;
+		}
+	}
+	
+	@Test
+	public void customSorting() {
+		final List<Integer> numbers = Arrays.asList(4, 7, 1, 6, 3, 5, 4);
+		final List<Integer> expected = Arrays.asList(7, 6, 5, 4, 4, 3, 1);
+		
+		Collections.sort(numbers, new ReverseNumericalOrder());
+		assertEquals(expected, numbers);
 	}
 }
